a65422ce096bfb58861c4533ae5069150bd336c8
dokimyj <doublekick132@gmail.com>
2020-11-09 02:30:03 +0900

    repo connected instead of direct pull

--- main.rb
+++ main.rb
@@ -5,9 +5,10 @@ uploaded_images = ''
 
 4.times do
   filename = Random.rand(1909)
-  ext = filename > 129 ? 'jpg' : 'png'
-  filepath = "./photos/#{filename}.#{ext}"
-  json_result = `twurl -X POST -H upload.twitter.com '#{api_path}' -f #{filepath} -F media`
+  filename += filename > 129 ? '.jpg' : '.png'
+  filepath = "https://raw.githubusercontent.com/dokimyj/twt_photo_repo/main/photos/#{filename}"
+  system("curl -o #{filename}.#{ext} #{filepath}")
+  json_result = `twurl -X POST -H upload.twitter.com '#{api_path}' -f #{filename} -F media`
   media_id = JSON.parse(json_result)['media_id']
   uploaded_images << "#{media_id},"
 end
--- photos/about.md
+++
@@ -1,9 +0,0 @@
-# Photograph
-
-- Inori Minase, a voice actress of Japan
-- All rights reserved by Inori Minase, will be deleted if there is any claim.
-- Excluding pictures from Inorimachi, the official fan club of Inori Minase.
-- changing all the file names into english due to encoding problem.
-
-- 1,894 files as of 2020/11/06
-- 1,910 files as of 2020/11/07
