b2acfaf506279f1ef2fd04e48df3093d8dbfa181
David Kim <doublekick132@gmail.com>
2020-11-09 15:18:51 +0900

    debug codes

--- main.rb
+++ main.rb
@@ -10,13 +10,18 @@ media_ids = ''
   uploaded_images << "#{filename}.#{ext}"
 end
 
+puts uploaded_images
+
 uploaded_images.uniq.each do |image|
   filepath = "https://raw.githubusercontent.com/dokimyj/twt_photo_repo/main/photos/#{image}"
   system("curl -o #{image} #{filepath}")
   json_result = `twurl -X POST -H upload.twitter.com '#{api_path}' -f #{image} -F media`
   media_id = JSON.parse(json_result)['media_id']
+  puts media_id
   media_id = "#{media_id}," if !media_id.to_s.empty?
   media_ids << media_id
 end
+
+puts media_ids
 
 system("twurl -d 'media_ids=#{media_ids}' -d 'status=#水瀬いのり' /1.1/statuses/update.json")
