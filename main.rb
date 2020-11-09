389521ab77ee86d3ac22455e6f1da7ce9a52eb3c
David Kim <doublekick132@gmail.com>
2020-11-09 10:18:51 +0900

    check if it shows multiple images

--- main.rb
+++ main.rb
@@ -1,18 +1,22 @@
 require 'json'
 
 api_path = '/1.1/media/upload.json?media_category=TWEET_IMAGE'
-uploaded_images = ''
+uploaded_images = []
+media_ids = ''
 
 4.times do
   filename = Random.rand(1909)
   ext = filename > 129 ? 'jpg' : 'png'
-  filename = "#{filename}.#{ext}"
-  filepath = "https://raw.githubusercontent.com/dokimyj/twt_photo_repo/main/photos/#{filename}"
-  system("curl -o #{filename} #{filepath}")
-  json_result = `twurl -X POST -H upload.twitter.com '#{api_path}' -f #{filename} -F media`
-  media_id = JSON.parse(json_result)['media_id']
-  media_id = "#{media_id}," if !media_id.to_s.empty?
-  uploaded_images << "#{media_id}"
+  uploaded_images << "#{filename}.#{ext}"
 end
 
-system("twurl -d 'media_ids=#{uploaded_images}' -d 'status=#水瀬いのり' /1.1/statuses/update.json")
+uploaded_images.uniq.each do |image|
+  filepath = "https://raw.githubusercontent.com/dokimyj/twt_photo_repo/main/photos/#{image}"
+  system("curl -o #{image} #{filepath}")
+  json_result = `twurl -X POST -H upload.twitter.com '#{api_path}' -f #{image} -F media`
+  media_id = JSON.parse(json_result)['media_id']
+  media_id = "#{media_id}," if !media_id.to_s.empty?
+  media_ids << media_id
+end
+
+system("twurl -d 'media_ids=#{media_ids}' -d 'status=#水瀬いのり' /1.1/statuses/update.json")
