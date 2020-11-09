b43cf7e97f8d8beb193658f7a60e3dd6fe980b62
David Kim <doublekick132@gmail.com>
2020-11-09 10:07:32 +0900

    comma made error...

--- main.rb
+++ main.rb
@@ -11,8 +11,8 @@ uploaded_images = ''
   system("curl -o #{filename} #{filepath}")
   json_result = `twurl -X POST -H upload.twitter.com '#{api_path}' -f #{filename} -F media`
   media_id = JSON.parse(json_result)['media_id']
-  puts "Is #{filename} invalid? #{media_id}"
-  uploaded_images << "#{media_id},"
+  media_id = "#{media_id}," if !media_id.empty?
+  uploaded_images << "#{media_id}"
 end
 
 system("twurl -d 'media_ids=#{uploaded_images}' -d 'status=#水瀬いのり' /1.1/statuses/update.json")
