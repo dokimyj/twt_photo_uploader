456e724009f2acb5c25227b08e7a43a3d07403ae
David Kim <doublekick132@gmail.com>
2020-11-18 02:34:23 +0900

    Update and rename jpgs.rb to main.rb

--- main.rb
+++ main.rb
@@ -3,14 +3,24 @@ require 'json'
 api_path = '/1.1/media/upload.json?media_category=TWEET_IMAGE'
 uploaded_images = ''
 
-4.times do
-  filename = Random.rand(0..1849)
-  filename = "#{filename}.jpg"
-  filepath = "https://raw.githubusercontent.com/dokimyj/twt_photo_repo/main/photos/jpgs/#{filename}"
+def upload_img(max_num, ext)
+  filename = Random.rand(0..max_num)
+  filename = "#{filename}.#{ext}"
+  filepath = "https://raw.githubusercontent.com/dokimyj/twt_photo_repo/main/photos/#{ext}s/#{filename}"
   system("curl -o #{filename} #{filepath}")
   json_result = `twurl -X POST -H upload.twitter.com '#{api_path}' -f #{filename} -F media`
   media_id = JSON.parse(json_result)['media_id']
   uploaded_images << "#{media_id},"
+end
+
+3.times do
+  upload_img(1849, jpg)
+end
+
+if Time.now.hour == 10 || Time.now.hour == 22
+  upload_img(137, png)
+else
+  upload_img(1849, jpg)
 end
 
 system("twurl -d 'media_ids=#{uploaded_images}' -d 'status=#水瀬いのり' /1.1/statuses/update.json")
