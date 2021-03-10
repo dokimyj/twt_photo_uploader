304757065a01912b54de34fc195403238e05a356
David Kim <doublekick132@gmail.com>
2021-03-11 00:49:21 +0900

    Project3.11

---
+++ project311.rb
@@ -0,0 +1,12 @@
+require 'net/http'
+
+def curl311
+  if Time.now.month == 3 && Time.now.day >= 10 && Time.now.hour >= 15
+    if Time.now.day <= 11 && Time.now.hour < 15
+      puts("3.11 東北大地震 #{Time.now.year - 2011}周年") 
+      puts("だらしない自分のコードが東北民の傷を治せますように :pray:") 
+      uri = URI('https://search.yahoo.co.jp/search?p=3.11')
+      http = Net::HTTP.get(uri)
+    end
+  end
+end
